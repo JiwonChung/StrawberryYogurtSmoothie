81cd66294f19a0ed6f96a6ad795ae79fe4df42a3
JiwonChung <jimmychung00@naver.com>
2021-09-13 05:28:29 +0900

    a

---
+++ Hopethisisntthelastchance/app/src/main/java/com/example/hopethisisntthelastchance/model/Artist.java
@@ -0,0 +1,26 @@
+package com.example.hopethisisntthelastchance.model;
+
+import java.util.Date;
+
+public class Artist {
+    // 아티스트 관리용 아이디
+    private long artist_id;
+
+    // 아티스트면서도 일반 유저임
+    private long user_id;
+
+    // 프로필 사진 링크
+    private String profile_imageLink;
+    // 닉네임
+    private String pseudonym; // 예명 아명
+
+    // 시간
+    private String timeRegion;
+    private Date birthday;
+    private Date debutDay;
+
+
+
+
+
+}
