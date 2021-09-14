c76165ede1af2e91344d080fbe5bac59749df4fb
JiwonChung <jimmychung00@naver.com>
2021-09-14 23:53:23 +0900

    a

---
+++ StrawberryYogurtSmoothie/app/src/main/java/com/example/StrawberryYogurtSmoothie/model/Comment.java
@@ -0,0 +1,13 @@
+package com.example.StrawberryYogurtSmoothie.model;
+
+import java.util.Date;
+
+public class Comment {
+    private long comment_id;
+
+    private long user_id;
+    private String text;
+
+    private String timeRegion;
+    private Date uploadDate;
+}
